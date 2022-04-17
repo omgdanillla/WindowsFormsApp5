ebbe8162d89f771d8b11eb27a53c1f11a9fdd523
Lenovo <Lenovo@DESKTOP-84CHVR1>
2022-04-17 20:12:50 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            private void Form1_MouseClick(object sender, MouseEventArgs e)
+            {
+                TextBox btn = new TextBox();
+                btn.Parent = this;
+                btn.Location = new Point(50, 70);
+            }
+            private void panel1_MouseClick(object sender, MouseEventArgs e)
+            {
+                Button btn = new Button();
+                btn.Location = new Point(70, 50);
+                panel1.Controls.Add(btn);
+            }
+
+        }
+    }
+
